fa93dda70703b9b39d1986b38f0caa151c18a042
Joachim Nolten <joachim@nolten.org>
2020-06-19 15:49:33 +0200

    Add specs and tweak timing for some series of single action.

--- expression-tracker/lib/recognizer.rb
+++ expression-tracker/lib/recognizer.rb
@@ -1,46 +1,54 @@
-class Recognizer
-  TIME_INTERVAL=1.3
+# frozen_string_literal: true
 
-  ACTION_UNITS =  { 
-      AU01: 'Inner brow raiser',
-      AU02: 'Outer brow raiser',
-      AU04: 'Brow lowerer',
-      AU05: 'Upper lid raiser',
-      AU06: 'Cheek raiser',
-      AU07: 'Lid tightener',
-      AU09: 'Nose wrinkler',
-      AU10: 'Upper lip raiser',
-      AU12: 'Lip corner puller',
-      AU14: 'Dimpler',
-      AU15: 'Lip corner depressor',
-      AU17: 'Chin raiser',
-      AU20: 'Lip stretcher',
-      AU23: 'Lip tightener',
-      AU25: 'Lips part',
-      AU26: 'Jaw drop',
-      AU28: 'Lip suck',
-      AU45: 'Blink' 
+# Class for identifying OpenFace Action Units
+class Recognizer
+  TIME_INTERVAL = 0.45
+
+  ACTION_UNITS =  {
+    AU01: 'Inner brow raiser',
+    AU02: 'Outer brow raiser',
+    AU04: 'Brow lowerer',
+    AU05: 'Upper lid raiser',
+    AU06: 'Cheek raiser',
+    AU07: 'Lid tightener',
+    AU09: 'Nose wrinkler',
+    AU10: 'Upper lip raiser',
+    AU12: 'Lip corner puller',
+    AU14: 'Dimpler',
+    AU15: 'Lip corner depressor',
+    AU17: 'Chin raiser',
+    AU20: 'Lip stretcher',
+    AU23: 'Lip tightener',
+    AU25: 'Lips part',
+    AU26: 'Jaw drop',
+    AU28: 'Lip suck',
+    AU45: 'Blink'
   }.freeze
 
   def initialize
     @last_action = 0
   end
 
-  def register_callback &block
+  def register_callback(&block)
     @callback = block
-  end 
+  end
 
-  def recognize values
-    if values['success'] > 0.9 && values['confidence'] > 0.92
-      if (values['timestamp'] - @last_action) > TIME_INTERVAL
-        if values['AU02_r'] > 3.3
-          @last_action = values['timestamp']
-          @callback.call :AU02
-        elsif values['AU12_r'] > 2.1
-          @last_action = values['timestamp']
-          @callback.call :AU12
-        end
-      end
+  def recognize(values)
+    return unless values['success'] > 0.9 && values['confidence'] > 0.92
+    puts "#{values['AU12_r']} #{values['timestamp'] - @last_action}" if ENV['DEBUG']
+    return unless (values['timestamp'] - @last_action) > TIME_INTERVAL
+
+    if au = action_unit(values)
+      @last_action = values['timestamp']
+      @callback.call au
+    end
+  end
+
+  def action_unit(values)
+    if values['AU02_r'] > 3.3
+      :AU02
+    elsif values['AU12_r'] > 2.1
+      :AU12
     end
   end
 end
